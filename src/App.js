3a7019d082296b7ad503862cc8306c9ddd4b9a7f
CBNaS <protocolcbr@gmail.com>
2020-03-06 15:50:41 -0600

    Correccion de errores

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { useEffect, useState } from "react";
 import "./css/style.css";
 
 export default function App() {
+
   const fecha  = new Date();
   let [datosClima, obtenerClima] = useState([]);
   let llave = "7b8f358532534d9b8e6940eeed5cc69a";
 
   let consultarAPI = async () => {
-    let url = `https://api.weatherbit.io/v2.0/current?city=Mexico&key=${llave}`;
+    let url = `https://api.weatherbit.io/v2.0/current?city=Pachuca&key=${llave}`;
     let respuesta = await fetch(url);
     let clima = await respuesta.json();
     obtenerClima(clima.data);
@@ -20,23 +21,23 @@ export default function App() {
     <div className="App">
       <div className = "container">
         <div>
-          <span class = "txtPrincipal">
+          <span className = "txtPrincipal">
             {fecha.getHours()+ " : "+fecha.getMinutes()}
           </span>
           <br/>
-          <span class = "txtSecundario">
+          <span className = "txtSecundario">
             {fecha.getDate()+ " de "+fecha.getMonth()+" de "+fecha.getFullYear()}
           </span>
         </div>
         <div>
-          <span class = "txtPrincipal" id = "temperatura">
+          <span className = "txtPrincipal" id = "temperatura">
             {datosClima.map((dato, key) => (
             <span key={key}>
-              {dato.temp +" °C"}
+              {dato.temp +"°C"}
             </span>
             ))}
           </span>
-          <span class = "txtSecundario" id = "estado">
+          <span className = "txtSecundario" id = "estado">
             Pachuca
           </span>
         </div>
