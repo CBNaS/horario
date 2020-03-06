03e7566c6b75b7dd950e55afb48a29130283b291
CBNaS <protocolcbr@gmail.com>
2020-03-06 13:51:46 -0600

    Consumiendo API para sacar consultar el clima

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import './App.css';
-import './css/style.css';
+import React, { useEffect, useState } from "react";
+import "./css/style.css";
 
-const fecha  = new Date();
+export default function App() {
+  const fecha  = new Date();
+  let [datosClima, obtenerClima] = useState([]);
+  let llave = "7b8f358532534d9b8e6940eeed5cc69a";
 
-function App() {
+  let consultarAPI = async () => {
+    let url = `https://api.weatherbit.io/v2.0/current?city=Mexico&key=${llave}`;
+    let respuesta = await fetch(url);
+    let clima = await respuesta.json();
+    obtenerClima(clima.data);
+  };
+  useEffect(() => {
+    consultarAPI();
+  }, []);
+
   return (
     <div className="App">
       <div className = "container">
@@ -19,7 +30,11 @@ function App() {
         </div>
         <div>
           <span class = "txtPrincipal" id = "temperatura">
-            31°
+            {datosClima.map((dato, key) => (
+            <span key={key}>
+              {dato.temp +" °C"}
+            </span>
+            ))}
           </span>
           <span class = "txtSecundario" id = "estado">
             Pachuca
@@ -29,5 +44,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
